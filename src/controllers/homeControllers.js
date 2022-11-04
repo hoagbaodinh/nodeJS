f36b4903baa66779607e2a70857c3e21e5bb4d2b
papinn <hoagbao.dinh@gmail.com>
2022-11-04 10:23:57 +0700

    create new user from input

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,4 +1,5 @@
 import db from "../models/index"
+import CRUDService from "../services/CRUDService"
 
 let getHomePage = async (req, res) => {
     try {
@@ -16,6 +17,15 @@ let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
+let getCRUD = (req, res) => {
+    return res.render('test/crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post CRUD from server');
+}
 
 // object : {
 //     key: '',
@@ -24,5 +34,7 @@ let getAboutPage = (req, res) => {
 
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD
 }
